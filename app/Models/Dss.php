b42970587d5ec0dd7ed1f8dd0a79e669add7b773
Bambang Adrian Sitompul <bambang.adrian@gmail.com>
2015-12-23 17:41:19 +0700

    Create the DSS required models

---
+++ app/Models/Dss.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MailMarketing\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Dss extends Model
+{
+    //
+}
---
+++ app/Models/DssCriteriaDetail.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MailMarketing\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DssCriteriaDetail extends Model
+{
+    //
+}
