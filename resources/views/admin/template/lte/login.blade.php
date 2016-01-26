6a04a7c9a6c41d53f56bc272ceff3c64c947b834
bambangadrian <bambang.adrian@gmail.com>
2016-01-26 22:44:33 +0700

    - Add new favicon

--- resources/views/admin/template/lte/login.blade.php
+++ resources/views/admin/template/lte/login.blade.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>{{ $pageTitle or 'Login' }}</title>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <link rel="shortcut icon" href="{{ asset('assets/img/favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" type="image/x-ico" href="{{ asset('/assets/img/favicon.ico') }}">
     <link rel="stylesheet" href="{{ asset("/vendor/bower_components/AdminLTE/bootstrap/css/bootstrap.min.css") }}">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
